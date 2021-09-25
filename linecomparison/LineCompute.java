08d7570869554bf2bea2acf02f781c581cb48bb1
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-25 08:03:52 +0530

    Added : Functionality to check Equality of Two Lines

--- linecomparison/LineCompute.java
+++ linecomparison/LineCompute.java
@@ -1,17 +1,17 @@
 package linecomparison;
 
 public class LineCompute {
-	double distance(int x1,int y1,int x2,int y2)	
+	double distanceCal(int x1,int y1,int x2,int y2)	
 	{
 	
 	         double dis;	
-		     dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-		     return dis;
+		     dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)); 	 	    	 	    
+             return dis;  	         
 	}
 public static void main(String[] args) {
-LineCompute obj = new LineCompute();
-double ans = obj.distance(1,2,3,4);
-System.out.println("distance between points is :"+ans);
+   LineCompute obj = new LineCompute();
+   Double line1=obj.distanceCal(1,1,1,1);
+   Double line2=obj.distanceCal(1,2,3,4);
+   System.out.println("line1 and line 2 are equal.True or False :" +line1.equals(line2));
 }
-	}
-
+}
